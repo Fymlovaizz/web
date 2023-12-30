d17841e98e4e8e0061d8d1d5e31569caf640b71f
Fymlovaizz <thaitinphat565@gmail.com>
2023-12-30 23:18:00 +0700

    fix link

--- functions/link.js
+++ functions/link.js
@@ -4,17 +4,17 @@ let aboutLink = document.getElementById("about-link"),
     indexLink = document.getElementById("index-link");
 
 aboutLink.addEventListener("click", () => {
-    window.open('../about.html', '_self');
+    window.open('about.html', '_self');
 })
 
 projectsLink.addEventListener("click", () => {
-    window.open('../project.html', '_self')
+    window.open('project.html', '_self')
 })
 
 schoolYearLink.addEventListener("click", () => {
-    window.open('../schoolYear.html', '_self')
+    window.open('schoolYear.html', '_self')
 })
 
 indexLink.addEventListener("click", () => {
-    window.open('../index.html', '_self')
+    window.open('index.html', '_self')
 })
